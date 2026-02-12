2e2546c421bd5325ea3f759e5f51939ce29c6675
anant garg <agarg44@ucsc.edu>
2026-02-11 17:01:27 -0800

    cache hit detection logic

--- src/main/scala/core/Cache.scala
+++ src/main/scala/core/Cache.scala
@@ -38,4 +38,54 @@ class Cache(c: Config) extends Module {
 
   val sIdle :: sLookup :: sCompare :: sRefill :: Nil = Enum(4)
   val state = RegInit(sIdle)
+
+  switch(state) {
+    is(sIdle) {
+      state := sLookup
+    }
+
+    is(sLookup) {
+      state := sCompare
+    }
+
+    is(sCompare) {
+      when(hit) {
+        state := sIdle
+      }.otherwise {
+        state := sRefill
+      }
+    }
+
+    is(sRefill) {
+      when(done) {
+        state := sIdle
+      }
+    }
+  }
+
+  val addr_reg = Reg(UInt(index_w.W))
+  val addr_tag_reg = Reg(UInt(tag_w.W))
+  when(state === sIdle){
+    addr_reg := addr_index
+    addr_tag_reg := addr_tag
+  }
+
+  val sram_index = Mux(state === sIdle, addr_index, addr_reg)
+  io.stall_cpu := (state === sLookup || state === sCompare || state === sRefill)
+
+  val tags_out = Wire(Vec(c.cacheAssociativity, UInt(tag_w.W)))
+  val tags_match = Wire(Vec(c.cacheAssociativity, Bool()))
+  val valid = Wire(Vec(c.cacheAssociativity, Bool()))
+  val hits = Wire(Vec(c.cacheAssociativity, Bool()))
+  for (i <- 0 until c.cacheAssociativity) {
+    tags_out(i) := tag_arrays(i).read(sram_index)
+
+    tags_match(i) := tags_out(i) === addr_tag_reg
+    valid(i) := valid_bit_array(i)(addr_reg)
+
+    hits(i) := tags_match(i) && valid(i)
+  }
+
+  val hit = hits.asUInt.orR
+
 }
